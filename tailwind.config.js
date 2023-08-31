7e748c1c1590727ecbe67c6a928e19e1634ab2d3
Chuli <tomasantunez.dev@gmail.com>
2023-08-31 20:19:48 -0300

    bases

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    container: {
+      padding: '2rem'
+    }
+  },
+  plugins: [],
+}
+
